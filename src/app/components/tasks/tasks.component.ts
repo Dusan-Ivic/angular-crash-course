60fb8bf87150bfbb490b72c8719b5847f77d3a07
Dusan-Ivic <ivicd99@gmail.com>
2023-09-24 18:20:01 +0200

    Task service with observables

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
-import { TASKS } from "../../data/mock-tasks";
+import { Component, OnInit } from "@angular/core";
 import { Task } from "../../types/Task";
+import { TaskService } from "../../services/task.service";
 
 @Component({
   selector: "app-tasks",
   templateUrl: "./tasks.component.html",
   styleUrls: ["./tasks.component.css"],
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
