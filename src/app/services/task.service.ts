6b1a84f5f67aac817ad4e713596a480a3f185e83
Dusan-Ivic <ivicd99@gmail.com>
2023-09-24 18:53:48 +0200

    Fake server with get, put and delete methods

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
-import { TASKS } from "../data/mock-tasks";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 import { Task } from "../types/Task";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json",
+  }),
+};
 
 @Injectable({
   providedIn: "root",
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = "http://localhost:5000/tasks";
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    return this.http.delete<Task>(`${this.apiUrl}/${task.id}`);
+  }
+
+  updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task, httpOptions);
   }
 }
